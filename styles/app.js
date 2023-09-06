fba339d854c69d1903eefdbb47be1542fbe2e915
Anthony Santiago <anthony.santiago58@yahoo.com>
2023-09-06 12:54:58 +1200

    add side nav on topic.html

---
+++ styles/app.js
@@ -0,0 +1,10 @@
+const sprintOne = document.querySelector('.one')
+const sprintTwo = document.querySelector('.two')
+const mainContainer = document.querySelector('section')
+sprintOne.addEventListener('click', function () {
+  mainContainer.textContent = "fdas"
+})
+
+sprintTwo.addEventListener('click', function () {
+  mainContainer.textContent = "asdf"
+})
