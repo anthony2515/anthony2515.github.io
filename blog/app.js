d67dad6459cbff50414280bf9b391ef5ae23616e
Anthony Santiago <anthony.santiago58@yahoo.com>
2023-09-10 17:55:00 +1200

    fix buttons going to nowhere

--- blog/app.js
+++ blog/app.js
@@ -6,6 +6,9 @@ const intro = document.querySelector(".intro")
 const whoami = document.querySelector(".whoami")
 const home = document.querySelector(".home")
 const buttonste = document.querySelectorAll(".te")
+const flex = document.querySelectorAll(".flex")
+const learning = document.querySelectorAll(".learning")
+const identity = document.querySelectorAll(".identity")
 
 home.addEventListener('click',function(){
 
@@ -103,9 +106,117 @@ buttonste[3].addEventListener('click', function() {
  
   const filePaths = 'learning-plan.html';
   window.location.href = filePaths;
+});
+/*
+flex[0].addEventListener('click', function() {
+ console.log("clicked")
+  const flexPath = '../te-houtaewa-template.html';
+  window.location.href = flexPath;
+
+
+
+ 
+});
+flex[1].addEventListener('click', function() {
+ 
+  const flexPath = 'flexbox.html';
+  window.location.href = flexPath;
+
+
+
+ 
+});
+flex[2].addEventListener('click', function() {
+ 
+  const flexPath = 'identity-values-strength.html';
+  window.location.href = flexPath;
+
+
+
+ 
+});
+flex[3].addEventListener('click', function() {
+ 
+  const flexPath = 'learning-plan.html';
+  window.location.href = flexPath;
 
 
 
  
 });
 
+identity[0].addEventListener('click', function() {
+ 
+  const identityPath = 'te-houtaewa-template.html';
+  window.location.href = identityPath;
+
+
+
+ 
+});
+identity[1].addEventListener('click', function() {
+ 
+  const identityPath = 'flexbox.html';
+  window.location.href = identityPath;
+
+
+
+ 
+});
+identity[2].addEventListener('click', function() {
+ 
+  const identityPath = 'identity-values-strength.html';
+  window.location.href = identityPath;
+
+
+
+ 
+});
+identity[3].addEventListener('click', function() {
+ 
+  const identityPath = 'learning-plan.html';
+  window.location.href = identityPath;
+
+
+
+ 
+});
+
+learning[0].addEventListener('click', function() {
+ 
+  const learningPath = 'te-houtaewa-template.html';
+  window.location.href = learningPath;
+
+
+
+ 
+});
+learning[1].addEventListener('click', function() {
+ 
+  const learningPath = 'flexbox.html';
+  window.location.href = learningPath;
+
+
+
+ 
+});
+learning[2].addEventListener('click', function() {
+ 
+  const learningPath = 'identity-values-strength.html';
+  window.location.href = learningPath;
+
+
+
+ 
+});
+learning[3].addEventListener('click', function() {
+ 
+  const learningPath = 'learning-plan.html';
+  window.location.href = learningPath;
+
+
+
+ 
+});
+
+*/
