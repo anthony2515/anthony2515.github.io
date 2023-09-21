76803cc6a4ddf537a9da43cd717a8e34a6f86010
Anthony Santiago <anthony.santiago58@yahoo.com>
2023-09-21 22:09:20 +1200

    i just need to expand the digits

--- portfolio/app.js
+++ portfolio/app.js
@@ -1,58 +1,43 @@
 const text = document.querySelector('input')
 const button = document.querySelector('button')
-const textArea = document.querySelector('textarea')
+const textArea = document.querySelector('textArea')
 const ones = ['','one','two','three','four','five','six','seven','eight','nine']
-//const teens = ['ten','eleven','twelve','thirteen','fourteen','fifteen','sixteen','seventeen','eigthteen','nineteen']
-const tens = ['','','twenty ','thirty ','fourty ','fifty ','sixty ','seventy ','eighty ','ninety ']
+const teens = ['ten','eleven','twelve','thirteen','fourteen','fifteen','sixteen','seventeen','eigthteen','nineteen']
+const tens = ['','ten','twenty','thirty','fourty','fifty','sixty','seventy','eighty','ninety']
+
+
 button.addEventListener('click',function(){
+
   let input = parseInt(text.value)
   textArea.value = convert(input)
 
-  function convert(number){
-    
-    if(number<10){
-      return ones[number]
-    }
-    else if(number > 9 && number < 20){
-      if(number ===10){
-          return "ten"
-      }
-      else if(number ===11){
-          return "eleven"
-      }
-      else if(number ===12){
-          return "twelve"
-      }
-      else if(number ===13){
-          return "thirteen"
-      }
-      else if(number == 15){
-          return "fifteen"
-      }
-      else if(number ===18){
-          return "eigthteen"
-      }
-      else{
-          return h1.textContent = `${ones[number - 10]}teen`
-      }
-    }
-    else if(number<100){//tens
-      return tens[Math.floor(number/10)]+ones[number%10]
-    }
-    else if(input<1000){//hundreds
-      return `${ones[Math.floor(number/100)]} hundred ${convert(number%100)}`
-    }
-    else if(input<10000){//thousands
-      return `${ones[Math.floor(number/1000)]} thousand ${convert(number%1000)}`
-    }
-    else{
-      return 'out of range'
-    }
-  }
-
- 
- 
-  
-  
 })
+
+function convert(number){
+    
+  if(number<10){
+    return ones[number]
+  }
+  else if(number < 20){
+    return teens[number%10]
+  }
+  else if(number<100){//tens
+    return `${tens[Math.floor(number/10)]} ${ones[number%10]}`
+  }
+  else if(number<1000){//hundreds
+   return `${ones[Math.floor(number/100)]} hundred ${convert(number%100)}`
+  }
+  else if(number<10000){//thousands
+    return `${ones[Math.floor(number/1000)]} thousand ${convert(number%1000)}`
+  }
+  else if(number<100000){//10 thousand
+    return `${convert(Math.floor(number/1000))} thousand ${convert(number%1000)}`
+  }
+  else if(number<1000000){//100 thousand
+    return `${ones[Math.floor(number/100000)]} hundred ${convert(number%100000)}`
+  }
+  else{
+    return 'out of range'
+  }
+}
 
