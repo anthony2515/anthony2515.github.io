5146d40fa3f6700c2f6fabd62beff0b126cd9be7
Anthony Santiago <anthony.santiago58@yahoo.com>
2023-09-21 16:56:06 +1200

    delete snake files due to complexity of implementing collision haha

---
+++ portfolio/app.js
@@ -0,0 +1,6 @@
+const text = document.querySelector('input')
+const button = document.querySelector('button')
+const textArea = document.querySelector('textarea')
+console.log(text,button,textArea)
+
+
